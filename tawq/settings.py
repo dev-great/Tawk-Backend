ec33dc09c768d8f5a1976dd1b71a441047a91ba3
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2024-11-20 20:54:40 +0100

    Update settings.py

--- tawq/settings.py
+++ tawq/settings.py
@@ -406,7 +406,7 @@ SLACK_REDIRECT_URI = '<your-slack-redirect-uri>'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
